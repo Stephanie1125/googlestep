32b523ba037b4662dc4fb83f9f5b122fc85f7fe0
Stephanie1125 <stephanieliu1125@gmail.com>
2016-06-28 03:15:54 +0900

    edit programs

--- week5/liuweek5hw2/week5_hw2.py
+++ week5/liuweek5hw2/week5_hw2.py
@@ -27,9 +27,12 @@ class HW2(webapp2.RequestHandler):
 class GUIDE(webapp2.RequestHandler):
     def get_line(self, target):
         """
-        find the set of lines where the target station is located
+        find a set of lines where the target station is located
         target: station(string)
         return: set
+        example:
+        target = '多摩川'
+        return: set([東横線/目黒線/多摩川線])
         """
         station_line = set([])
         url = 'http://test-mojo.appspot.com/net?format=json'
@@ -40,39 +43,67 @@ class GUIDE(webapp2.RequestHandler):
                     station_line.add(dictionary['Name'])
         return station_line
 
-    def get_dict(self, line):  # return a diction where the line is
+    def get_dict(self, line):
+        """
+        find a dict where the line is located
+        line: train line(string)
+        return: dict
+        example:
+        line = '山手線'
+        return: {"Name":"山手線","Stations":["品川","大崎","五反田","目黒","恵比寿","渋谷","原宿"...]}
+        """
         url = 'http://test-mojo.appspot.com/net?format=json'
         output = json.load(urllib.urlopen(url))
         for dictionary in output:
             if dictionary['Name'] == line:
                 return dictionary
 
-    def get_index(self, target, list):  # return index
+    def get_index(self, target, list):
+        """
+        return the index where the target is in a list
+        """
         for index in xrange(len(list)):
             if list[index] == target:
                 return index
 
-    def check_station(self, start, end):  # check if two station are in the same line
+    def check_station(self, start, end):
+        """
+        check if two station is in the same line
+        start, end: stations(strings)
+        return: intersection line (list)
+        """
         start_set = self.get_line(start)
         end_set = self.get_line(end)
         intersection_set_lst = [str(item) for item in (start_set & end_set)]
         return intersection_set_lst
 
-    def check_line(self, curr_lines, dest):
+    def check_line(self, curr_lines, end):
+        """
+        check if the destination inside the current line
+        return True or False
+        """
         for line in curr_lines:
-            if dest in self.get_dict(line)["Stations"]:
+            if end in self.get_dict(line)['Stations']:
                 return True
         return False
 
     def adjunt_line(self, line):
+        """
+        line: train line(string)
+        return: intersection line (set)
+        """
         line_set = set()
         line_dictionary = self.get_dict(line)
         for station in line_dictionary['Stations']:
             for line in self.get_line(station):
                 line_set.add(line)
-        return line_set  # return a set with intersection line
+        return line_set
 
     def intersection_station(self, line_a, line_b):
+        """
+        line_a, line_b: two different line (strings)
+        return intersection station (list)
+        """
         dictionary_a = self.get_dict(line_a)
         dictionary_b = self.get_dict(line_b)
         set_a = set()
@@ -82,7 +113,19 @@ class GUIDE(webapp2.RequestHandler):
         for station in dictionary_b['Stations']:
             set_b.add(station)
         intersection_station = [str(item) for item in (set_a & set_b)]
-        return intersection_station  # a list of station
+        return intersection_station
+
+    def intersection_line(self, curr_line):
+        """
+        return a set of lines where the current line is intersection with
+        curr_line: current line(string)
+        return: set
+        """
+        current_dict = self.get_dict(curr_line)
+        intersection_lines_set = set()
+        for station in current_dict['Stations']:
+            intersection_lines_set |= self.get_line(station)
+        return intersection_lines_set
 
     def get(self):
         # get start and end
@@ -93,43 +136,84 @@ class GUIDE(webapp2.RequestHandler):
         end = self.request.get("to")
         start_line_str = '/'.join([str(item) for item in self.get_line(start)])
         end_line_str = '/'.join([str(item) for item in self.get_line(end)])
-        self.response.write('from %s[%s] to %s[%s]<br>' % (start, start_line_str, end, end_line_str))
+        self.response.write('From %s[%s] to %s[%s]' % (start, start_line_str, end, end_line_str))
         # calculate path
+        self.print_path_sameline(start, end)
+        self.transfer_station(end, start)
 
+        # for station in station_to_trans:
+        #     self.print_path(start, station)
+        #     self.print_path(station, end)
 
-        curr_lines = self.get_line(start)
-        count = 0
-        # while not self.check_line(curr_lines, end):
-        #     next_lines = set()
-        #     for line in curr_lines:
-        #         next_lines += self.adjunt_line(line)
-        #     curr_lines = next_lines
-        #     count += 1
-        #
-        #
+    def transfer_station(self, end, start):
+        line_to_take = self.transfer_line(start, end)
+        station_to_trans = []
+        for line_index in xrange(1, len(line_to_take)):
+            trans_station = self.intersection_station(line_to_take[line_index - 1], line_to_take[line_index])
+            station_to_trans += trans_station
+        if len(station_to_trans) != 0:
+            self.response.write('Transfer Station(s): ')
+            for station in station_to_trans:
+                    self.response.write(' %s (乗換)' % station)
+            self.response.write('<hr>')
 
-            # start_set_line = self.get_line(start)
-            # start_list = []
-            # for line in start_set_line:
-            #     start_list.append(self.adjunt_line(line))
-            # end_set_line = self.get_line(end)
+    def transfer_line(self, start, end):
+        curr_lines = self.get_line(start)  # initial start line (set)
+        count = 0  # 乗り換え times
+        line_path = []
+        while not self.check_line(curr_lines, end):
+            line_path.append(curr_lines)
+            next_lines = set()
+            for line in curr_lines:
+                next_lines |= self.adjunt_line(line)  # possible lines that you can connect to
+            curr_lines = next_lines
+            count += 1
+        end_line_set = self.get_line(end)
+        line_to_take = []
+        end_line_to_take = ''.join([str(item) for item in (end_line_set & curr_lines)])  # initial
+        line_to_take.append(end_line_to_take)
+        for path_set in line_path[::-1]:
+            end_line_to_take = ''.join(str(item) for item in (path_set & self.intersection_line(end_line_to_take)))
+            line_to_take.append(end_line_to_take)
+        line_to_take.reverse()
+        if count != 0:
+            self.response.write('Recommand to take ')
+            for line in line_to_take:
+                self.response.write(' >> %s' % line)
+            self.response.write('(乗換 %d 回)<br><hr>' % count)
+        return line_to_take
 
-    #     intersection_line = (self.get_line(start) & self.get_line(end))
-    #     self.print_path(end, intersection_line, start) # if in the same line
-    # def print_path(self, end, intersection_line, start):
-    #     now = datetime.now()
-    #     self.response.write('<br> Starting at: %s' % start)  # utc time + 9 hours = JP time
-    #     self.response.write('@ %s-%s-%s ' % (now.year, now.month, now.day))
-    #     self.response.write(' %s:%s:%s JST <br>' % (now.hour + 9, now.minute, now.second))
-    #     for line in intersection_line:
-    #         start_index = self.get_index(start, self.get_dict(line)['Stations'])
-    #         end_index = self.get_index(end, self.get_dict(line)['Stations'])
-    #         if start_index < end_index:
-    #             for station in self.get_dict(line)['Stations'][start_index:end_index + 1]:
-    #                 self.response.write('>> %s <br>' % station)
-    #         else:
-    #             for station in self.get_dict(line)['Stations'][end_index:start_index + 1]:
-    #                 self.response.write('>> %s' % station)
+    def print_path_sameline(self, start, end):
+        """
+        when start and destination staitons are on the same line (乗換なし)
+        """
+        intersection_line = (self.get_line(start) & self.get_line(end))
+        now = datetime.now()
+        self.response.write('<br> Starting at: %s' % start)  # utc time + 9 hours = JP time
+        self.response.write('@ %s-%s-%s ' % (now.year, now.month, now.day))
+        self.response.write(' %s:%s:%s JST <br><hr>' % (now.hour + 9, now.minute, now.second))
+        if len(intersection_line) != 0:
+            intersection_line_str = ''.join([str(item) for item in intersection_line])
+            self.response.write('Recommand to take %s (乗換なし) <br>' % intersection_line_str)
+        self.print_path(start, end)
+
+    def print_path(self, start, end):
+        intersection_line = (self.get_line(start) & self.get_line(end))
+        for line in intersection_line:
+            start_index = self.get_index(start, self.get_dict(line)['Stations'])
+            end_index = self.get_index(end, self.get_dict(line)['Stations'])
+            if start_index < end_index:
+                path = self.get_dict(line)['Stations'][start_index:end_index + 1]
+                for station in path:
+                    self.response.write('>> %s ' % station)
+                self.response.write('<hr>')
+            else:
+                path = self.get_dict(line)['Stations'][end_index:start_index + 1]
+                path.reverse()
+                for station in path:
+                    self.response.write('>> %s ' % station)
+                self.response.write('<hr>')
+
 
 app = webapp2.WSGIApplication([
     ('/', HW2),
