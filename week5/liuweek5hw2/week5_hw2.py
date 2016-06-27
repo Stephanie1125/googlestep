aa319a3b595bd8b0f6a1fdb6225fcf4610fd26c3
Stephanie1125 <stephanieliu1125@gmail.com>
2016-06-27 11:46:12 +0900

    edit programs

--- week5/liuweek5hw2/week5_hw2.py
+++ week5/liuweek5hw2/week5_hw2.py
@@ -4,6 +4,8 @@ import jinja2
 import json
 import urllib
 from datetime import datetime
+# import pytz
+# Failed to set up library pytz for GAE: ImportError: No module named pytz
 
 JINJA_ENVIRONMENT = jinja2.Environment(
         loader=jinja2.FileSystemLoader(os.path.dirname(__file__)),
@@ -25,22 +27,34 @@ class GUIDE(webapp2.RequestHandler):
         start = self.request.get("from")
         end = self.request.get("to")
         url = 'http://test-mojo.appspot.com/net?format=json'
-        output = json.load(urllib.urlopen(url))
-        for i in output: # i is a dict
-            for j in i["Stations"]:
-                if j == start:
-                    self.response.write('from: %s[%s]' % (start, i["Name"]))
-                    break
-        for i in output: # i is a dict
-            for j in i["Stations"]:
-                if j == end:
-                    self.response.write(' to: %s[%s],<br>' % (end, i["Name"]))
-                    break
+        output = json.load(urllib.urlopen(url))  # output is a list
+
+        for dictionary in output:
+            for station_index in xrange(len(dictionary["Stations"])):
+                if dictionary["Stations"][station_index] == start:
+                    start_dict = dictionary
+                    start_line = start_dict["Name"]
+                    start_index = station_index
+                if dictionary["Stations"][station_index] == end:
+                    end_dict = dictionary
+                    end_line = end_dict["Name"]
+                    end_index = station_index
+
+        self.response.write('from %s[%s] %d to %s[%s] %d' % (start, start_line, start_index, end, end_line, end_index))
+
         now = datetime.now()
-        self.response.write('<br> Starting at >> %s' % (start)) # utc time + 9 hours = JP time
-        self.response.write('@ %s-%s-%s %s:%s:%s JST <br>'% (now.year, now.month, now.day, now.hour + 9, now.minute, now.second))
+        self.response.write('<br> Starting at: %s' % start)  # utc time + 9 hours = JP time
+        self.response.write('@ %s-%s-%s ' % (now.year, now.month, now.day))
+        self.response.write(' %s:%s:%s JST <br>' % (now.hour + 9, now.minute, now.second))
+
+        if start_line == end_line:
+            if start_index > end_index:
+                for station in start_dict["Stations"][start_index:end_index + 1]:
+                    self.response.write(' >> %s' % station)
+            for station in start_dict["Stations"][start_index:end_index - 1:-1]:
+                self.response.write(' >> %s' % station)
 
 app = webapp2.WSGIApplication([
     ('/', HW2),
-    ('/guidance?',GUIDE),
+    ('/guidance?', GUIDE),
 ], debug=True)
